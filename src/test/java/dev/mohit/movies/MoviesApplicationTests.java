8e95047841e2a0b8f8b50b563af916b64528eeac
Mohit Patil <patilmohit551@gmail.com>
2024-07-31 00:22:59 +0530

    Boiler plate with basic get all movies and get movie by ID functionality

---
+++ src/test/java/dev/mohit/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mohit.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
